1bfd7b75129f80da6a7f576bb8bf9a30c9c49f70
SkFeatures <shaikh.sabbir.ali0@gmail.com>
2026-09-03 13:20:56 +0600

    Remove primary email from allowed emails list
    Removed a primary email from the allowed emails list.

--- user.js
+++ user.js
@@ -4,7 +4,6 @@
 
 // যেসব ইমেইল আইডি অ্যাপে লগইন করতে পারবে, তাদের তালিকা নিচে দিন:
 const ALLOWED_EMAILS = [
-    "nurullairri@gmail.com",       // আপনার প্রধান ইমেইল
     "s.ali@harvestplus.solutions"
     // "s.ali@harvestplus.solutions", // ভবিষ্যতে কাউকে অ্যাক্সেস দিতে চাইলে এভাবে কমা দিয়ে যুক্ত করবেন
     // "colleague@gmail.com"
